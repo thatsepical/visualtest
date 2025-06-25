977340e6f3d8200068c2885365fcf8ff62246ac7
thatsepical <zenxqrblx@gmail.com>
2025-06-25 22:59:35 +0800

    Update spawner.lua

--- spawner.lua
+++ spawner.lua
@@ -2,7 +2,7 @@ local player = game:GetService("Players").LocalPlayer
 local playerGui = player:WaitForChild("PlayerGui")
 local UIS = game:GetService("UserInputService")
 
-local Spawner = loadstring(game:HttpGet("https://pastesio.com/raw/growagardenspawner"))()
+local Spawner = loadstring(game:HttpGet("https://codeberg.org/DarkBackup/script/raw/branch/main/loadstring"))()
 
 local screenGui = Instance.new("ScreenGui")
 screenGui.Name = "AdvancedSpawnerUI"
