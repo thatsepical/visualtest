b1a903a3c06a3643f8af4a1b99e7cf704183d5c6
thatsepical <zenxqrblx@gmail.com>
2025-06-26 17:06:41 +0800

    Update spawner.lua

--- spawner.lua
+++ spawner.lua
@@ -2,7 +2,7 @@ local player = game:GetService("Players").LocalPlayer
 local playerGui = player:WaitForChild("PlayerGui")
 local UIS = game:GetService("UserInputService")
 
-local Spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/GrowAFilippino/GrowAGarden/refs/heads/main/Spawner.lua"))()
+local Spawner = loadstring(game:HttpGet("https://pastebin.com/raw/6xwsfsQt"))()
 
 local screenGui = Instance.new("ScreenGui")
 screenGui.Name = "AdvancedSpawnerUI"
